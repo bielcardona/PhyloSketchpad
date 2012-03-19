6ff167810fd73d8b0b7d953944e4a0a77dd29412
Biel Cardona <bielcardona@gmail.com>
2012-03-19 19:39:13 +0100

    added support for relative paths changed to phylonetwork module

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, request, jsonify#, session
+from flask import Flask, render_template, request, jsonify, session
 import json
-from phylonetwork import PhyloNetwork as pn
-import phylonetwork.distances as distances
-from phylonetwork.generators import random_tree_generator as tgr
+from PhyloNetwork import PhyloNetwork as pn
+import PhyloNetwork.distances as distances
 import networkx as nx
 import time
-import os
-
-os.environ['PATH'] = '/usr/local/bin:' + os.environ['PATH']
 
 app = Flask(__name__)
 net = None
@@ -22,26 +18,20 @@ def prova():
 
 @app.route('/dump',methods=['POST'])
 def dump():
-    #print session['eNewick']
-    #net = pn(eNewick=session['eNewick'])
+    print session['eNewick']
+    net = pn(eNewick=session['eNewick'])
     print net
     return jsonify({})
 
 def applyAll(fs,u):
-    return '\n\n'.join([command+':'+str(fs[command](u)) for command in fs])
+    return '<br>'.join([command+':'+str(fs[command](u)) for command in fs])
 
 def applyAllNets(fsnets,net,subnetworks,distances_methods):
-    print 'aquibis'
-    data1 = '\n\n'.join([command+':'+str(getattr(net,str(command))()) for command in fsnets])
+    data1 = '<br>'.join([command+':'+str(getattr(net,str(command))()) for command in fsnets])
     for dm in distances_methods:
         for net2 in subnetworks:
             if net != net2:
-                print 'aquitris'
-                #print mu_distance(net,net2)
-                #print net.eNewick(),net2.eNewick(),distances_methods[dm]
-                #print distances_methods[dm](net,net2)
-
-                data1 += '\n\n'+dm+' to '+net2.name+': '+str(distances_methods[dm](net,net2))
+                data1 += '<br>'+dm+' to '+net2.name+': '+str(distances_methods[dm](net,net2))
     return data1
 
 @app.route("/processNetwork",methods=['POST'])
@@ -63,19 +53,17 @@ def processNetwork():
             #print node
             net.add_node(str(node['id']));
             if ('label' in node) and (node['label']!=None):
-                print node['id'],node['label']
                 net._labels[str(node['id'])] = str(node['label'])
         for edge in edges:
             net.add_edge(str(edge['source']),str(edge['target']))
     
         subnetworks = nx.weakly_connected_component_subgraphs(net)
+        print subnetworks;
         for subnetwork in subnetworks:
-            #print 'sbn:'+subnetwork.eNewick()
             for node in subnetwork.nodes():
                 if node in net._labels:
                     subnetwork._labels[node] = net._labels[node]
-            print 'sbn.'+subnetwork.eNewick()
-        print subnetworks;
+            print subnetwork
         for network in networks:
             #print network['name']
             #print network['nodes']
@@ -83,25 +71,23 @@ def processNetwork():
             for subnetwork in subnetworks:
                 if onenote  in subnetwork.nodes():
                     subnetwork.name = network['name']
+        
         fs = {command:getattr(net,str(command)) for command in commands}
         data = {}
         data['nodes'] = {u:applyAll(fs,u) for u in net.nodes()}
     
         #fsnets = {command_network:getattr(net,str(command_network)) for command_network in commands_networks}
-        print 'aqui'
         data['networks'] = {n.name:applyAllNets(commands_networks,n,subnetworks,distances_methods) for n in subnetworks}
         #print datanetworks
         
         return jsonify(response = data);
-    except Exception, err:
-        print err
-        return jsonify(response={'error':'Some error occurred. Please chech your data. If you think this is a bug, please contact us (see About section).<br> Error message: %s' % err})
+    except:
+        return jsonify(response={'error':'Some error occurred. Please chech your data. If you think this is a bug, please contact us (see About section)'})
 
         
 @app.route('/fromEnewick',methods=['POST'])
 def fromEnewick():
-    #try:
-    if True:
+    try:
         mydata = request.json
         eNewick=mydata["eNewick"]
         offsetId = mydata["offsetId"]
@@ -109,49 +95,34 @@ def fromEnewick():
         offsety = mydata["offsety"]
         net = pn()
         net._lastlabel = offsetId
-        net._from_eNewick(eNewick)
-        #session['eNewick'] = eNewick
+        net.from_eNewick(eNewick)
+        #time.sleep(6)
+        session['eNewick'] = eNewick
         pos = nx.graphviz_layout(net, 'dot')
         minx = min([pos[u][0] for u in net.nodes()])
         miny = min([pos[u][1] for u in net.nodes()])
         offsettoapplyx = minx - offsetx - 50
         offsettoapplyy = miny - offsety
+        #===========================================================================
+        # xs = [p[0] for p in pos.values()]
+        # ys = [p[1] for p in pos.values()]
+        # medx = (max(xs)+min(xs))/2
+        # medy = (max(ys)+min(ys))/2
+        # #print pos
+        # #print medx,medy
+        # posnor = {u:(pos[u][0]-medx,pos[u][1]-medy) for u in pos}
+        #===========================================================================
+        #print posnor
         nodes = [{'id':u,'label': net.label(u), 'x':pos[u][0]-offsettoapplyx, 'y':-pos[u][1]+offsettoapplyy} for u in net.nodes()]
         edges = [{'source':edge[0],'target':edge[1], 'type': 'arrow'} for edge in net.edges()]
         dict = {'nodes':nodes,'edges':edges}
+        #print dict
+        #print dict
         return jsonify(response=dict)
-    #except Exception, err:
-        return jsonify(response={'error':'Some error occurred. Please chech your data. If you think this is a bug, please contact us (see About section)<br> Error message: %s' % err})
+    except:
+        return jsonify(response={'error':'Some error occurred. Please chech your data. If you think this is a bug, please contact us (see About section)'})
 
-@app.route('/getRandom',methods=['POST'])
-def getRandom():
-    try:
-        mydata = request.json
-        n=mydata["n"]
-        binary=mydata["binary"]
-        nested_taxa=mydata["nested_taxa"]
-        taxa = [str(i+1) for i in range(n)]
-        offsetId = mydata["offsetId"]
-        offsetx = mydata["offsetx"]
-        offsety = mydata["offsety"]
-        tg = tgr(taxa, binary=binary, nested_taxa=nested_taxa, id_offset=offsetId)
-        net = tg.next()
-        #net = pn()
-        #net._lastlabel = offsetId
-        #net.from_eNewick(eNewick)
-        #session['eNewick'] = eNewick
-        pos = nx.graphviz_layout(net, 'dot')
-        minx = min([pos[u][0] for u in net.nodes()])
-        miny = min([pos[u][1] for u in net.nodes()])
-        offsettoapplyx = minx - offsetx - 50
-        offsettoapplyy = miny - offsety
-        nodes = [{'id':u,'label': net.label(u), 'x':pos[u][0]-offsettoapplyx, 'y':-pos[u][1]+offsettoapplyy} for u in net.nodes()]
-        edges = [{'source':edge[0],'target':edge[1], 'type': 'arrow'} for edge in net.edges()]
-        dict = {'nodes':nodes,'edges':edges}
-        return jsonify(response=dict)
-    except Exception, err:
-        return jsonify(response={'error':'Some error occurred. Please chech your data. If you think this is a bug, please contact us (see About section)\n Error message: %s' % err})
-
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
     app.run(debug=True)
